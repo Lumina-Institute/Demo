f906ffec14c6e46da279920789526f5321f89d20
jamiiefoxx <foxxiceberg@gmail.com>
2023-01-10 14:42:13 +0000

    deploy action v4

---
+++ tasks.py
@@ -0,0 +1,19 @@
+import inspect
+
+if not hasattr(inspect, 'getargspec'):
+    inspect.getargspec = inspect.getfullargspec
+
+from invoke import task, Collection
+
+
+@task(name="deploy")
+def builds(c):
+    print("Root is connected to VPS")
+    c.run("cd /var/www/Demo")
+    c.run("git pull origin main")
+    c.run("python3 -m venv .env")
+    c.run("source .env/bin/activate")
+    c.run("pip install --upgrade pip")
+    c.run("pip install -r requierement.txt")
+    c.run("gunicorn main:app --bind 0.0.0.0:5001 --reload")
+    print("is working")
